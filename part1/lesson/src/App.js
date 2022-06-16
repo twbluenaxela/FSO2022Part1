8d28d3baacf88b5792516f82cf03190077c55c9c
Nick <rednaxela64@outlook.com>
2022-06-16 22:30:15 +0000

    Refactor to make time counter app

--- part1/lesson/src/App.js
+++ part1/lesson/src/App.js
@@ -1,34 +1,8 @@
-const Hello = (props) => {
+const App = (props) => {
+  const {counter} = props
   return (
-    <div>
-      <p>Hello {props.name}, you are {props.age} years old</p>
-    </div>
-  )
-} 
-
-const Footer = () => {
-  return (
-    <div>
-      <p>本React App由 <a href="https://github.com/twbluenaxela">twbluenaxela</a>制作设计</p>
-    </div>
+    <div>{counter}</div>
   )
 }
-
-const App = () => {
-
-  const name = "Peter"
-  const age = 10
-
-  return (
-    <div>
-      <h1>Greetings</h1>
-      <Hello name="Daisy" age={age} />
-      <Hello name={name} age={50 + 10} />
-      <Footer />
-    </div>
-  )
-  
-}
-  
 
 export default App
