de059bfd3a7a84d7b5523dde5fb30be50c889d9e
Nick <rednaxela64@outlook.com>
2022-06-24 20:15:39 +0000

    Refactor lesson components

--- part1/lesson/src/App.js
+++ part1/lesson/src/App.js
@@ -1,7 +1,38 @@
-const App = (props) => {
-  const {counter} = props
+import { useState } from 'react'
+
+const Display = ({counter}) => <div>{counter}</div>
+
+const Button = ({onClick, text}) =>(
+    <button onClick={onClick} >
+      {text}
+    </button>
+  )
+
+const App = () => {
+  const [ counter, setCounter ] = useState(0)
+
+  console.log("rendering..." + counter)
+
+  const increaseByOne = () => setCounter(counter + 1)
+  const decreaseByOne = () => setCounter(counter - 1)
+  const setToZero = () => setCounter(0)
+
   return (
-    <div>{counter}</div>
+    <div>
+      <Display counter={counter} />
+      <Button 
+        onClick={increaseByOne}
+        text='plus'
+      />
+      <Button 
+        onClick={setToZero}
+        text='zero'
+      />
+      <Button 
+        onClick={decreaseByOne}
+        text='minus'
+      />
+    </div>
   )
 }
 
