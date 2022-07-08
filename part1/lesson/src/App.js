73c6c8093247b3a8b7458aea850778f2c8641bdb
阿洋 <rednaxela64@outlook.com>
2022-07-08 20:13:48 +0000

    Add button press history

--- part1/lesson/src/App.js
+++ part1/lesson/src/App.js
@@ -1,39 +1,44 @@
-import { useState } from 'react'
+import { useState } from "react";
 
-const Display = ({counter}) => <div>{counter}</div>
-
-const Button = ({onClick, text}) =>(
-    <button onClick={onClick} >
-      {text}
-    </button>
-  )
-
-const App = () => {
-  const [ counter, setCounter ] = useState(0)
-
-  console.log("rendering..." + counter)
-
-  const increaseByOne = () => setCounter(counter + 1)
-  const decreaseByOne = () => setCounter(counter - 1)
-  const setToZero = () => setCounter(0)
-
+const History = (props) => {
+  if(props.allClicks.length === 0) {
+    return(
+      <div>
+        the app is used by pressing the buttons
+      </div>
+    )
+  }
   return (
     <div>
-      <Display counter={counter} />
-      <Button 
-        onClick={increaseByOne}
-        text='plus'
-      />
-      <Button 
-        onClick={setToZero}
-        text='zero'
-      />
-      <Button 
-        onClick={decreaseByOne}
-        text='minus'
-      />
+      button press history: {props.allClicks.join(' ')}
     </div>
   )
 }
 
-export default App
+const App = () => {
+  const [left, setLeft] = useState(0)
+  const [right, setRight] = useState(0)
+  const [allClicks, setAll] = useState([])
+
+  const handleLeftClick = () => {
+    setAll(allClicks.concat('L'))
+    setLeft(left + 1)
+  }
+
+  const handleRightClick = () => {
+    setAll(allClicks.concat('R'))
+    setRight(right + 1)
+  }
+
+  return (
+    <div>
+      {left}
+      <button onClick={handleLeftClick}>left</button>
+      <button onClick={handleRightClick}>right</button>
+      {right}
+      <History allClicks={allClicks}/>
+    </div>
+  );
+};
+
+export default App;
